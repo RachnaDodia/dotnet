fdac824fa70b832708c56fbb91d653820734d3a1
RrPpDd <rachna.dodia@gmail.com>
2020-10-14 11:27:42 +0530

    Create myapp.cs

---
+++ myapp.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myApp
+{
+    class Program1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to .net core application using Ansible");
+        }
+    }
+}
